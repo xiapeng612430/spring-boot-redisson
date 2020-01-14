be55e5cd3df2f7b2e77ddcc3c57aaf853da4f3e6
夏先鹏 <951256916@qq.com>
2020-01-14 14:05:15 +0800

    zset

--- src/main/java/com/example/redisson/controller/TestController.java
+++ src/main/java/com/example/redisson/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.example.redisson.controller;
 
 import org.redisson.api.RAtomicLong;
+import org.redisson.api.RScoredSortedSet;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,5 +27,25 @@ public class TestController {
         // 同步执行方式
         myLong.compareAndSet(3, 401);
 
+    }
+
+    @GetMapping("/RZset")
+    public void rzet() {
+        RScoredSortedSet<String> myZset = redissonClient.getScoredSortedSet("myZset");
+        myZset.add(100, "t1");
+        myZset.add(200, "t2");
+        myZset.add(300, "t3");
+        myZset.add(400, "t4");
+        myZset.add(500, "t5");
+        myZset.add(600, "t6");
+        myZset.add(700, "t7");
+        myZset.add(800, "t8");
+        for (int i = 0; i < 5; i++) {
+            new Thread(() -> {
+                String s = myZset.pollFirst();
+                System.out.println(s);
+
+            }).start();
+        }
     }
 }
